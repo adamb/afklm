b967fd7012e26ff92214ce2851bbf3ea11b0d8d3
Adam Beguelin <adam@beguelin.com>
2024-07-22 13:34:47 +0200

    use REWARD as bookingFlow

--- run_curl.sh
+++ run_curl.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+# Enable debug mode
+# set -x
+
 # Replace the following line with your actual curl command
 
 curl -X POST 'https://api.airfranceklm.com/opendata/offers/v1/best-fare-offers' \
 -H 'accept: application/json' \
 -H "API-Key: ${API_KEY}" \
--H 'Content-Type: application/json' \
+-H 'Content-Type: application/hal+json' \
 -d '{
   "commercialCabin": "PREMIUM",
-  "bookingFlow": "LEISURE",
-  "promoType": "AWARD_TICKET",
+  "bookingFlow": "REWARD",
   "passengers": [
     {
       "id": 1,
@@ -38,10 +40,12 @@ curl -X POST 'https://api.airfranceklm.com/opendata/offers/v1/best-fare-offers' 
       }
     }
   ],
-  "dateInterval": "2024-08-01/2024-08-08",
+  "dateInterval": "2024-08-01/2024-12-08",
   "filters": {
     "marketingCarriers": [
       "AF"
     ]
   }
 }'
+
+
